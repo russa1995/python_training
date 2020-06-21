9bda519843773cc264a018fbd63fc42643ed8160
Ruslan <ruslankhaliullov@gmail.com>
2020-06-21 17:46:31 +0300

    созданы пакеты для разных зон ответственности

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
-from contact import Contact
+from model.contact import Contact
 
 class TestAddContact(unittest.TestCase):
     def setUp(self):
