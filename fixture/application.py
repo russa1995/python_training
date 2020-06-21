ea6443db4d9bdc5798a200952c611111b710f5c6
Ruslan <ruslankhaliullov@gmail.com>
2020-06-21 18:17:24 +0300

    extracted session class

--- fixture/application.py
+++ fixture/application.py
@@ -1,23 +1,22 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 class Application:
 
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
-    def logout(self):
-        wd = self.wd
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-
-    def retun_to_group_page(self):
-        wd = self.wd
-        # return to group page
-        wd.find_element_by_link_text("groups").click()
+    def open_home_page(self):
+        wd = self.app.wd
+        wd.get("https://localhost/addressbook/")
 
     def group_create(self, group):
         wd = self.wd
+        # return to group page
+        self.open_home_page()
+        wd.find_element_by_link_text("groups").click()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -32,21 +31,13 @@ class Application:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.open_groups_page()
 
     def open_groups_page(self):
         wd = self.wd
         # open groups page
         wd.find_element_by_link_text("groups").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -11,17 +11,11 @@ def app(request):
     return fixture
 
 def test_add_empty_group(app):
-    app.open_home_page()
-    app.login( username="admin", password="secret")
-    app.open_groups_page()
+    app.session.login( username="admin", password="secret")
     app.group_create( Group(name="", header="", footer=""))
-    app.retun_to_group_page()
-    app.logout()
+    app.session.logout()
 
 def test_add_group(app):
-    app.open_home_page()
-    app.login( username="admin", password="secret")
-    app.open_groups_page()
+    app.session.login( username="admin", password="secret")
     app.group_create( Group(name="griou", header="ytu", footer="rtyw"))
-    app.retun_to_group_page()
-    app.logout()
+    app.session.logout()
