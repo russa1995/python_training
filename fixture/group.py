8f4acf021c9b55b866985287b882b8dd5f7cfc47
Ruslan <ruslankhaliullov@gmail.com>
2020-06-27 10:45:17 +0300

    общая фикстура для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -24,6 +24,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.open_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_name("delete").click()
+        self.open_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         # open groups page
