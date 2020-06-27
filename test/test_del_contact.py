7fc9c5871b6f9d5df8d6b8ea3177f097d71906c5
Ruslan <ruslankhaliullov@gmail.com>
2020-06-27 19:44:17 +0300

    fixture validation and changes in login logout

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,3 +1,6 @@
+from model.contact import Contact
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(Contact(firstname="test"))
     app.contact.delete_first_contact()
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,6 +1,8 @@
 from model.contact import Contact
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(Contact(firstname="test"))
     app.contact.edit_first_contact(Contact(firstname="new", middlename="new", lastname="new", nickname="new", title="new", company="new",
                     address="new", home="new", mobile="new", work="new",
                     fax="new", email="new", email2="new", email3="new", homepage="new", bday="1",
