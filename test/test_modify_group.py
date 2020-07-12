9c191dabba54115f2132c2d8fce729569be6454a
Ruslan <ruslankhaliullov@gmail.com>
2020-07-12 12:08:35 +0300

    check by list, lesson 4

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,11 +1,21 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    group.id = old_groups[0].id
     if app.group.count() == 0:
         app.group.group_create(Group(name="test"))
-    app.group.modify_first_group( Group(name="New group"))
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups)  == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-def test_modify_group_header(app):
-    if app.group.count() == 0:
-        app.group.group_create(Group(name="test"))
-    app.group.modify_first_group( Group(header="New group"))
+#def test_modify_group_header(app):
+#    old_groups = app.group.get_group_list()
+#    if app.group.count() == 0:
+#        app.group.group_create(Group(name="test"))
+#    app.group.modify_first_group( Group(header="New group"))
+#    new_groups = app.group.get_group_list()
+#    assert len(old_groups)  == len(new_groups)
