9dfafcf0ac0c7ea1ef863a26013d5d5bcef36ed8
Ruslan <ruslankhaliullov@gmail.com>
2020-07-31 15:19:40 +0300

    generate test data in json

--- generator/group.py
+++ generator/group.py
@@ -13,16 +13,25 @@ except getopt.GetoptError as err:
     getopt.usage()
     sys.exit(2)
 
+n=5
+f="data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
 def random_string(prefix, maxlen):
     symbol = string.ascii_letters + string.digits + string.punctuation +" "*10
     return prefix +  "".join(random.choice(symbol) for i in range(random.randrange(maxlen)))
 
 testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range(5)
+    for i in range(n)
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
+with open(file, "w") as out:
     f.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
