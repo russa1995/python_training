d91d919e3b727ea26d29ea87d4a50aadcd66f37d
Ruslan <ruslankhaliullov@gmail.com>
2020-08-01 15:04:15 +0300

    ORM first step

---
+++ fixture/orm.py
@@ -0,0 +1,32 @@
+from pony.orm import *
+from datetime import datetime
+from pymysql.converters import decoders
+
+
+class ORMFixture:
+
+    db = Database()
+
+    class ORMGroup(db.Entity):
+        _table_ = 'group_list'
+        id = PrimaryKey(int, column='group_id')
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        firstname = Optional(str, column='firstname')
+        lastname = Optional(str, column='lastname')
+        deprecated = Optional(datetime, column='deprecated')
+
+    def __init__(self, host, name, user, password):
+        self.db.bind('mysql', host=host, database=name, user=user, password=password, conv=decoders)
+        self.db.generate_mapping()
+
+    @db_session
+    def get_group_list(self):
+        with db_session:
+            return select(g for g in ORMFixture.ORMGroup)
+
