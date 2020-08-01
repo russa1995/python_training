ecb0bd6d9a4bcd559138e944376000f8c1b3a783
Ruslan <ruslankhaliullov@gmail.com>
2020-08-01 16:11:23 +0300

    test contacts_in_group

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,12 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
-db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", user="root", db="addressbook", password="")
 
 try:
-    l = db.get_contact_list()
+    l = db.get_contacts_not_in_group(Group(id="76"))
     for item in l:
         print(item)
     print(len(l))
 finally:
-    pass # db.destroy()
+    pass #db.destroy
