758b28cec666cf1f2ce356ea10a7e1b79a759fa1
Ruslan <ruslankhaliullov@gmail.com>
2020-08-01 21:13:00 +0300

    Excercise 21

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,13 +1,16 @@
 import re
+from model.contact import Contact
 
-def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.address == contact_from_edit_page.address
-    assert contact_from_home_page.email == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+def test_phones_on_home_page(app, db):
+    contact_from_home_page = sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
+    contact_from_db = sorted(db.get_contact_list(), key=Contact.id_or_max)
+    for i in range(len(contact_from_home_page)):
+        assert clear(contact_from_home_page[i].address) == clear(contact_from_db[i].address)
+        assert clear(contact_from_home_page[i].firstname) == clear(contact_from_db[i].firstname)
+        assert clear(contact_from_home_page[i].lastname) == clear(contact_from_db[i].lastname)
+        assert clear(contact_from_home_page[i].all_phones_from_home_page) == clear(merge_phones_like_on_home_page(contact_from_db[i]))
+        assert clear(contact_from_home_page[i].email) == clear(merge_emails_like_on_home_page(contact_from_db[i]))
+
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
